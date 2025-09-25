49e5ef3b98557ce0eb72b651aeb1f1c2774c7b3b
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-09-24 21:08:01 -0400

    Setting up project structure

--- main.cpp
+++ main.cpp
@@ -7,3 +7,30 @@
 * NYU School of Engineering Policies and Procedures on
 * Academic Misconduct.
 **/
+
+#include "CS3113/cs3113.h"
+
+
+// Global Constants
+constexpr int   SCREEN_WIDTH  = 1600,
+                SCREEN_HEIGHT = 900,
+                FPS           = 60,
+                SIZE          = 1000;
+
+constexpr char    BG_COLOUR[] = "#B2AAC6";
+constexpr Vector2 ORIGIN      = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+constexpr Vector2 BASE_SIZE   = { (float) SIZE, (float) SIZE };
+
+constexpr float LIMIT_ANGLE = 20.0f;  // when turning directions will flip
+
+// Global Variables
+AppStatus gAppStatus = RUNNING;
+
+Texture2D gTexture;
+
+// Function Declarations
+void initialise();
+void processInput();
+void update();
+void render();
+void shutdown();
