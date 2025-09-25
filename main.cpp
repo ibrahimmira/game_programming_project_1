c8ca9c4a53b32be34d02677f1a449f0669afad5c
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-09-24 21:15:04 -0400

    Simple background update

--- main.cpp
+++ main.cpp
@@ -34,3 +34,47 @@ void processInput();
 void update();
 void render();
 void shutdown();
+
+// Function Definitions
+void initialise()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Textures");
+
+    SetTargetFPS(FPS);
+}
+
+void processInput() 
+{
+    if (WindowShouldClose()) gAppStatus = TERMINATED;
+}
+
+void update() 
+{
+    // Nothing to update
+}
+
+void render()
+{
+    BeginDrawing();
+    ClearBackground(ColorFromHex(BG_COLOUR));
+    EndDrawing();
+}
+
+void shutdown() { CloseWindow(); }
+
+int main(void)
+{
+    initialise();
+
+    while (gAppStatus == RUNNING)
+    {
+        processInput();
+        update();
+        render();
+    }
+
+    shutdown();
+
+    return 0;
+}
+
