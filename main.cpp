6e7af50164c238aae5d5547b19381f31b0d678bd
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-09-24 20:52:28 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+/**
+* Author: Ibrahim Mira
+* Assignment: Simple 2D Scene
+* Date due: 2025-09-27, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
