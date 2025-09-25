c1e26891b6b859c41e229f4edbca9bf65c977c7e
ibrahimmira <135983387+ibrahimmira@users.noreply.github.com>
2025-09-24 22:00:48 -0400

    Object integration

--- main.cpp
+++ main.cpp
@@ -15,16 +15,28 @@
 constexpr int   SCREEN_WIDTH  = 1600,
                 SCREEN_HEIGHT = 900,
                 FPS           = 60,
-                SIZE          = 1000;
+                SIZE          = 300;
 
-constexpr char    BG_COLOUR[] = "#B2AAC6";
+constexpr char    BG_COLOUR[] = "#710000ff";
 constexpr Vector2 ORIGIN      = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
 constexpr Vector2 BASE_SIZE   = { (float) SIZE, (float) SIZE };
 
-constexpr float LIMIT_ANGLE = 20.0f;  // when turning directions will flip
+constexpr Vector2 REGERA_SIZE  = { (float) 1200, (float) 673 };
+constexpr Vector2 FLASH_SIZE   = { (float) 1024, (float) 1202 };
+constexpr Vector2 CAMERA_SIZE  = { (float) 965, (float) 723 };
+
+constexpr char REGERA[]  = "assets/regera.png";
+constexpr char FLASH[]  = "assets/flash.png";
+constexpr char CAMERA[]  = "assets/camera.png";
 
 // Global Variables
-AppStatus gAppStatus = RUNNING;
+AppStatus gAppStatus     = RUNNING;
+float     gScaleFactor   = SIZE,
+          gAngle         = 0.0f,
+          gPulseTime     = 0.0f;
+Vector2   gPosition      = ORIGIN;
+Vector2   gScale         = CAMERA_SIZE;
+float     gPreviousTicks = 0.0f;
 
 Texture2D gTexture;
 
@@ -39,6 +51,8 @@ void shutdown();
 void initialise()
 {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Textures");
+
+    gTexture = LoadTexture(CAMERA);
 
     SetTargetFPS(FPS);
 }
@@ -57,6 +71,41 @@ void render()
 {
     BeginDrawing();
     ClearBackground(ColorFromHex(BG_COLOUR));
+
+    // Whole texture (UV coordinates)
+    Rectangle textureArea = {
+        // top-left corner
+        0.0f, 0.0f,
+
+        // bottom-right corner (of texture)
+        static_cast<float>(gTexture.width),
+        static_cast<float>(gTexture.height)
+    };
+
+    // Destination rectangle – centred on gPosition
+    Rectangle destinationArea = {
+        gPosition.x,
+        gPosition.y,
+        static_cast<float>(gScale.x),
+        static_cast<float>(gScale.y)
+    };
+
+    // Origin inside the source texture (centre of the texture)
+    Vector2 objectOrigin = {
+        static_cast<float>(gScale.x) / 2.0f,
+        static_cast<float>(gScale.y) / 2.0f
+    };
+
+    // Render the texture on screen
+    DrawTexturePro(
+        gTexture, 
+        textureArea, 
+        destinationArea, 
+        objectOrigin, 
+        gAngle, 
+        WHITE
+    );
+
     EndDrawing();
 }
 
